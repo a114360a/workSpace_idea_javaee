41c8d7ca32d3af9785af3d2d8329093aa992ee53
aji <a114360a@163.com>
2021-06-21 10:50:43 +0800

    third commit hot-fix

--- day_04/src/com/aji/exec/StringExec.java
+++ day_04/src/com/aji/exec/StringExec.java
@@ -102,6 +102,7 @@ public class StringExec {
         //System.out.println(s1.indexOf(s2));
         System.out.println("aa");
         System.out.println("bb");
+        System.out.println("222222");
         System.out.println("hello hot-fix");
     }
 }
